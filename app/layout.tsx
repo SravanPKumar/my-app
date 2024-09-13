4bf47d1a9e1619b533109153bd2a15bab0526162
SravanPKumar <112685716+SravanPKumar@users.noreply.github.com>
2024-09-14 00:54:58 +0530

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
 // components
 import Header from "@/components/Header";
-import PageTransition from "@/components/Pagetransition";
-import StairTransition from "@/components/Stairtransition";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
-const jetbrainsMono = JetBrains_Mono({subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800"], variable: '--font-jetbrainsMono'});
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: '--font-jetbrainsMono'
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,12 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={jetbrainsMono.variable}>
-          <Header />
-          <StairTransition />
-          <PageTransition>{children}</PageTransition>
-          
+      <body className={jetbrainsMono.variable}>
+        <Header />
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
